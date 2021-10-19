687033b6323282ee6d9fe0eb87bcae10ebd74e4f
litchy is he <72008587+lithu-lithish@users.noreply.github.com>
2021-10-19 15:58:28 +0530

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,7 +10,7 @@ return (
 
   <nav class="navbar navbar-expand-lg navbar-dark">
 
-    <a class="navbar-brand" href="">tindog</a>
+    <a class="navbar-brand" href="">SINGLE DEBT<span><h6>STABILITY SECURITY STRESS-FREE</h6></span></a>
 
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02">
         <span class="navbar-toggler-icon"></span>
@@ -19,14 +19,23 @@ return (
     <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 
       <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+          <a class="nav-link" href="#footer">DebtSolution</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">Bounced Cheques</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#footer">Lawyers & Credit Scores</a>
+        </li>
         <li class="nav-item">
           <a class="nav-link" href="#footer">Contact</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#pricing">Pricing</a>
+          <a class="nav-link" href="#footer">Blog</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#cta">Download</a>
+          <a class="nav-link" href="#footer">Login</a>
         </li>
       </ul>
 
@@ -35,20 +44,54 @@ return (
 
 
   {/* <!-- Title --> */}
+  <section class="colored-section">
 
-  <div class="row">
+<div id="testimonials-carousel" class="carousel slide" data-ride="false">
+  <div class="carousel-inner">
+    <div class="carousel-item active container-fluid">
+    <div class="row">
 
-    <div class="col-lg-6">
-      <h1 class="big-heading">Meet new and interesting dogs nearby.</h1>
-      <button type="button" class="btn btn-dark btn-lg download-button"><i class="fab fa-apple"></i> Download</button>
-      <button type="button" class="btn btn-outline-light btn-lg download-button"><i class="fab fa-google-play"></i> Download</button>
+<div class="col-lg-6">
+  <h1 class="big-heading">Debt Advice and Solutions</h1> <h6> SingleDebt have helped thousands of customers in Europe and India, 
+    that’s why we’ll only ever suggest a debt management solution</h6>
+  <button type="button" class="btn btn-dark btn-lg download-button"> Debt solution</button>
+  <button type="button" class="btn btn-outline-light btn-lg download-button"> Contact us</button>
+</div>
+
+<div class="col-lg-6">
+  <img class="title-image" src="assets/images/fin.jpg" alt="iphone-mockup" />
+</div>
+
+</div>
     </div>
+    <div class="carousel-item container-fluid">
+    <div class="row">
 
-    <div class="col-lg-6">
-      <img class="title-image" src="assets/images/iphone6.png" alt="iphone-mockup" />
+<div class="col-lg-6">
+  <h1 class="big-heading">Debt Advice and Solutions</h1> <h6> SingleDebt have helped thousands of customers in Europe and India, 
+    that’s why we’ll only ever suggest a debt management solution</h6>
+  <button type="button" class="btn btn-dark btn-lg download-button"> Debt solution</button>
+  <button type="button" class="btn btn-outline-light btn-lg download-button"> Contact us</button>
+</div>
+
+<div class="col-lg-6">
+  <img class="title-image" src="assets/images/fin.jpg" alt="iphone-mockup" />
+</div>
+
+</div>
     </div>
-
   </div>
+  <a class="carousel-control-prev" href="#testimonials-carousel" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#testimonials-carousel" role="button" data-slide="next">
+<span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+</section>
+
+  
 
 </div>
 
@@ -62,22 +105,85 @@ return (
 <div class="container-fluid">
 
   <div class="row">
-    <div class="feature-box col-lg-4">
+    {/* <div class="feature-box">
       <i class="icon fas fa-check-circle fa-4x"></i>
-      <h3 class="feature-title">Easy to use.</h3>
-      <p>So easy to use, even your dog could do it.</p>
+      <h3 class="feature-title"><span class = "notation"> how ?</span> we can help</h3>
+      <ul>
+     <li> <p>We are here to help you get your debts in control</p> </li>
+     <li> <p>Are you worried about your mounting debt?</p></li>
+     <li><p>Unable to sleep?</p></li>
+     <li><p>Being chased by your creditors?</p></li>
+     <li><p> Not sure how to get your finances back on track?</p></li>
+     <li><p>Bounced/returned cheques?</p></li>
+     <li><p>Pending legal actions?</p></li>
+     <li><p>Low credit score</p></li>
+     <li><p>No one to turn to for help?</p></li>
+      </ul>
+    </div> */}
+
+<div class="feature-box col-lg-6">
+    <i class="icon fas fa-check-circle fa-4x"></i>
+      <h3 class="feature-title"><span class = "notation"> how ?</span> we can help</h3>
+      <ul>
+     <li> <p>We are here to help you get your debts in control</p> </li>
+     <li> <p>Are you worried about your mounting debt?</p></li>
+     <li><p>Unable to sleep?</p></li>
+     <li><p>Being chased by your creditors?</p></li>
+     <li><p> Not sure how to get your finances back on track?</p></li>
+     <li><p>Bounced/returned cheques?</p></li>
+     <li><p>Pending legal actions?</p></li>
+     <li><p>Low credit score</p></li>
+     <li><p>No one to turn to for help?</p></li>
+      </ul>
     </div>
 
-    <div class="feature-box col-lg-4">
+    <div class="feature-box col-lg-6">
       <i class="icon fas fa-bullseye fa-4x"></i>
-      <h3 class="feature-title">Elite Clientele</h3>
-      <p>We have all the dogs, the greatest dogs.</p>
+      <h3 class="feature-title"><span class = "notation">Our</span> Services</h3>
+      <p>It makes managing your personal finances like a cakewalk. Be the master of your money so you can get more out of life.</p>
+       <bold>Debt Advice and Solutions</bold><p>SingleDebt have helped thousands of customers in Europe and India, that’s why we’ll only ever suggest a debt management solution that we know, is right for you..</p>
+       <bold>Debt Management Plan</bold><p>One of the solutions that SingleDebt is offering is a payment plan that will let you pay off all your unsecured debts in one affordable rate. This is called a Debt Management Plan (DMP)</p>
+    </div>
+        
+
+    <div class="feature-box col-lg-6">
+      <i class="icon fas fa-heart fa-4x"></i>
+      <h3 class="feature-title"><span class = "notation">LEGAL</span> ADVISOR</h3>
+      <p>Our legal advisors know how stressful debt is. They understand that people get into debt for a number of reasons – these could be things like the loss of income or just the ever increasing cost of living. They know it’s often nothing to do with people being careless with their money.
+        Also we are here to assist you legally on any returned cheque issues that you might have. We never charge you any upfront fees for initial advice, so Fill in the form and one of our legal advisors will contact you.</p>
+        <bold>We could help you manage any of these unsecured debts:</bold>
+        <ul>
+     <li> <p>Debt Collectors</p> </li>
+     <li> <p>Personal Loan</p></li>
+     <li><p>Education loan</p></li>
+     <li><p>Credit Cards</p></li>
+     <li><p>Unsecured Loans & Overdrafts</p></li>
+     <li><p>Household bills & Utilities</p></li>
+     <li><p>Returned Cheques</p></li>
+     <li><p>Improve your Credit Score</p></li>
+     <li><p>Credit Card Settlement</p></li>
+      </ul>
+
     </div>
 
-    <div class="feature-box col-lg-4">
-      <i class="icon fas fa-heart fa-4x"></i>
-      <h3 class="feature-title">Guaranteed to work.</h3>
-      <p>Find the love of your dog's life or your money back.</p>
+    <div class="feature-box col-lg-6">
+    <img class="icon" src="https://img.icons8.com/ios/50/000000/puzzle.png"/>
+      <h3 class="feature-title"><span class = "notation">What</span> our customers says</h3>
+      <p>Our legal advisors know how stressful debt is. They understand that people get into debt for a number of reasons – these could be things like the loss of income or just the ever increasing cost of living. They know it’s often nothing to do with people being careless with their money.
+        Also we are here to assist you legally on any returned cheque issues that you might have. We never charge you any upfront fees for initial advice, so Fill in the form and one of our legal advisors will contact you.</p>
+        <bold>We could help you manage any of these unsecured debts:</bold>
+        <ul>
+     <li> <p>Debt Collectors</p> </li>
+     <li> <p>Personal Loan</p></li>
+     <li><p>Education loan</p></li>
+     <li><p>Credit Cards</p></li>
+     <li><p>Unsecured Loans & Overdrafts</p></li>
+     <li><p>Household bills & Utilities</p></li>
+     <li><p>Returned Cheques</p></li>
+     <li><p>Improve your Credit Score</p></li>
+     <li><p>Credit Card Settlement</p></li>
+      </ul>
+
     </div>
   </div>
 
@@ -92,14 +198,15 @@ return (
 
 <div id="testimonial-carousel" class="carousel slide" data-ride="false">
   <div class="carousel-inner">
+    <h1><center>Testimonials</center></h1><center><h6>Gladly our clients think we are great. Here is what they have to say about us.</h6></center>
     <div class="carousel-item active container-fluid">
-      <h2 class="testimonial-text">I no longer have to sniff other dogs for love. I've found the hottest Corgi on TinDog. Woof.</h2>
-      <img class="testimonial-image" src="assets/images/dog-img.jpg" alt="dog-profile"/>
+      <h2 class="testimonial-text">I no longer have to worry about the query, I received all the answer here in one place.</h2>
+      {/* <img class="testimonial-image" src="assets/images/dog-img.jpg" alt="dog-profile"/> */}
       <em>Pebbles, New York</em>
     </div>
     <div class="carousel-item container-fluid">
-      <h2 class="testimonial-text">My dog used to be so lonely, but with TinDog's help, they've found the love of their life. I think.</h2>
-      <img class="testimonial-image" src="assets/images/lady-img.jpg" alt="lady-profile"/>
+      <h2 class="testimonial-text">Great services here in this place, i am very glad to be here as a customer</h2>
+      {/* <img class="testimonial-image" src="assets/images/lady-img.jpg" alt="lady-profile"/> */}
       <em>Beverly, Illinois</em>
     </div>
   </div>
@@ -117,10 +224,10 @@ return (
 {/* <!-- Press --> */}
 
 <section class="colored-section" id="press">
-<img class="press-logo" src="assets/images/techcrunch.png" alt="tc-logo"/>
+{/* <img class="press-logo" src="assets/images/techcrunch.png" alt="tc-logo"/>
 <img class="press-logo" src="assets/images/tnw.png" alt="tnw-logo"/>
 <img class="press-logo" src="assets/images/bizinsider.png" alt="biz-insider-logo"/>
-<img class="press-logo" src="assets/images/mashable.png" alt="mashable-logo"/>
+<img class="press-logo" src="assets/images/mashable.png" alt="mashable-logo"/> */}
 
 </section>
 
@@ -129,7 +236,7 @@ return (
 
 <section class="white-section" id="pricing">
 
-<h2 class="section-heading">A Plan for Every Dog's Needs</h2>
+{/* <h2 class="section-heading">A Plan for Every Dog's Needs</h2>
 <p>Simple and affordable price plans for your and your dog.</p>
 
 <div class="row">
@@ -183,7 +290,7 @@ return (
 
 
 
-</div>
+</div> */}
 
 </section>
 
@@ -192,26 +299,70 @@ return (
 
 <section class="colored-section" id="cta">
 
-<div class="container-fluid">
+{/* <div class="container-fluid">
 
   <h3 class="big-heading">Find the True Love of Your Dog's Life Today.</h3>
   <button class="download-button btn btn-lg btn-dark" type="button"><i class="fab fa-apple"></i> Download</button>
   <button class="download-button btn btn-lg brn-light" type="button"><i class="fab fa-google-play"></i> Download</button>
-</div>
+</div> */}
 </section>
+
+
 
 
 {/* <!-- Footer --> */}
 
 <footer class="white-section" id="footer">
+    <h2>MORE DETAILS</h2>
+
+<table>
+  
+ 
+  <tr>
+    <td>SINGLE DEBT</td>
+    <td>USEFUL LINKS  </td>
+   
+  </tr>
+  
+  <tr>
+    <td>Single Debt have helped thousands of customers in India,<br></br>
+      that’s why we’ll only ever suggest a debt management solution that we know,<br></br>
+      is right for you.<br></br>
+      CIN: AAN6607<br></br>
+      PAN: AAOFC0728P<br></br>
+      GST: 27AAOFC0728P1ZY<br></br>
+
+      </td>
+    <td><a class="nav-link" href="#footer">DebtSolution</a>
+    <a class="nav-link" href="#footer">DebtSolution</a>
+    <a class="nav-link" href="#footer">Bounced cheques</a>
+    <a class="nav-link" href="#footer">Lawyers & Credit Scores</a>
+    <a class="nav-link" href="#footer">Contact</a>
+    <a class="nav-link" href="#footer">Blog</a>
+    <a class="nav-link" href="#footer">Login</a>
+    </td>
+  
+  </tr>
+  
+  <tr>
+    <td>Magazzini Alimentari Riuniti</td>
+    <td></td>
+
+  </tr>
+</table>
+
+
 <div class="container-fluid">
-  <i class="social-icon fab fa-facebook-f"></i>
-  <i class="social-icon fab fa-twitter"></i>
-  <i class="social-icon fab fa-instagram"></i>
-  <i class="social-icon fas fa-envelope"></i>
-  <p>© Copyright 2021 TinDog [by Litchee]</p>
-</div>
-</footer>
+      
+      <i class="social-icon fab fa-facebook-f"></i>
+      <i class="social-icon fab fa-twitter"></i>
+      <i class="social-icon fab fa-instagram"></i>
+      <i class="social-icon fas fa-envelope"></i>
+      {/* <p>© Copyright 2021 single debt</p> */}
+      <p>FOLLOW US ON</p>
+    </div>
+    
+  </footer>
     </React.Fragment>
 )
 }
